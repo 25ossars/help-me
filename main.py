3cecbb5d329deb73479290287394e7060fe1d023
Sara <93925344+25ossars@users.noreply.github.com>
2022-03-28 10:56:59 -0400

    Create main.py
    Added the actual code.

---
+++ main.py
@@ -0,0 +1,154 @@
+import time
+time.sleep(2)
+print("Hello there! \nBottom Text")
+time.sleep(2)
+print("My name is Sara.")
+time.sleep(2)
+print("...")
+time.sleep(2)
+print("Well, not really. I'm just a computer but that's not important.")
+time.sleep(2)
+print("ANYWAY")
+time.sleep(2)
+print("What is your name?")
+name1 = input("My name is... ")
+print(name1)
+if name1 == "Sara":
+  print("Ayyyyyyy, same. It's a good name to have.")
+if name1 == "Jesse":
+  print("Good evening, watermelon.")
+if name1 == "Rowena":
+  print("Hello, cookie master.")
+if name1 == "Mary":
+  print("Brorther? Is that you?")
+if name1 == "Marie":
+  print("Would you happen to be a member of the Squid Sisters?")
+  answer1 = input("Yes or No? ")
+  print( answer1)
+  if answer1 == "No":
+    print("Understandable, have a great day.")
+  if answer1 == "Yes":
+    print("Awesome! I hope this stupid thing is fresh enough for you.")
+if name1 == "Jacob":
+  print("Bebo, föther.")
+if name1 == "Pearl":
+  print("Team Chaos for life!!!")
+if name1 == "Marina":
+  print("Team Ketchup for life!!!")
+if name1 == "Aelish":
+  print("OMG, it's my favorite cousin!")
+if name1 == "Link":
+  print("Yahaha, I found you!")
+if name1 == "Louisa":
+  print("Hello, fellow robot person.")
+if name1 == "Callie":
+  print("Would you happen to be a member of the Squid Sisters?")
+  print(answer1)
+  if answer1 == "No":
+    print("Understandable, have a great day.")
+  if answer1 == "Yes":
+    print("Awesome! I hope this stupid thing is fresh enough for you.")
+else:
+  print(str(name1) + ", huh? That's a fun name.")
+time.sleep(3)
+print("You're probably wondering what this is, aren't you?")
+time.sleep(2)
+print("Your guess is as good as mine.")
+time.sleep(2)
+print("Actually, I do know. It's a stupid test that asks stupid questions.")
+time.sleep(2.5)
+print("I've taken about 3 classes on Python and decided to make it everyone else's problem.")
+time.sleep(3)
+print("Wanna hear something weird my friend just said?")
+time.sleep(3)
+jesse = input("Yes/ No ")
+if jesse == "Yes":
+  print("'Yeetily deet, my mask is on my feet'")
+  time.sleep(2)
+  print("...")
+  time.sleep(2)
+  print("Yeah, I'm worried about him too.")
+if jesse == "No":
+  print("Me neither.")
+time.sleep(2)
+print("Aaaaaaanyway, time to start the test part.")
+time.sleep(2.5)
+print("First, it's time to take the PacerGram Fitness test.")
+time.sleep(2)
+print("The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.")
+time.sleep(6)
+print("Did you run 99 laps in that time?")
+answer2 = input("Yes/ No ")
+print(answer2)
+if answer2 == "Yes":
+  print("Whaddaya want, a medal?")
+if answer2 == "No":
+  print("Then perish.")
+time.sleep(3)
+print("Time for another test, but no running this time.")
+time.sleep(2)
+print("Do you like frogs?")
+answer3 = input("Yes/ No ")
+print(answer3)
+if answer3 == "Yes":
+  print("Test passed, we can be friends.")
+if answer3 == "No":
+  print("Test failed. We cannot be friends.")
+time.sleep(2)
+print("Question 2, steal the spice trade. \n A. That's not a question, but the Dutch did it anyway. \n B. Huh?")
+q1 = input("Type in the letter you choose, capitalize please. ")
+print(q1)
+if q1 == "A":
+  print("Ah, I see you are a person of culture as well.")
+if q1 == "B":
+  print("Go watch 'History of the entire world, i guess', it's an absolute masterpiece.")
+time.sleep(2.5)
+print("Time for an actual question.")
+time.sleep(2)
+print("Frogs are... \n A. friends :) \n B. the greatest things ever \n C. going to take over the world and I'll join them \n D. all of the above")
+select1 = input("Type the letter you choose, capitalize please. ")
+print(select1)
+if select1 == "A":
+  print("Yes. Yes they are.")
+if select1 == "B":
+  print("Absolutely.")
+if select1 == "C":
+  print("Same, but don't tell anyone about the frog invasion")
+if select1 == "D":
+  print("All answers are correct, but this is the correctest one. Is that a word? I don't care. You are big brained, my friend.")
+time.sleep(4)
+print("Seriously, do not tell anyone about the frog invasion or I will steal your kneecaps.")
+time.sleep(2.5)
+print("But if you truly want to help with the frog invasion, go take the frog test.")
+time.sleep(2.5)
+print("Ok, last question I swear.")
+time.sleep(2)
+print("Did you like this thing?")
+answer4 = input("Yes/ No ")
+time.sleep(2)
+if answer4 == "No":
+  print("Completely understandable, I agree.")
+if answer4 == "Yes":
+  print("Thanks, but why?")
+time.sleep(2)
+print("In all seriousness, thank you for putting up with this nonsense. Absolutely no braincells were used in the making of this atrocity. Text me if you have any suggestions.")
+time.sleep(4)
+print("Credits:\n Coding: Sara \n Emotional support/ food/ tech support/ testing: Jesse \n Not kicking me out while I worked: The Brookline Library/ Jesse once again \n Ideas/ tech support/ testing: Louisa \n Teaching me how to use Python: Mr. Petty \n Testing/ Generally being an awesome person: Aelish \n Testing: Mary \n Testing: Mom \n Testing: Dad")
+time.sleep(5)
+print("Okie bye, thanks again for doing this when you could have done literally anything else.")
+time.sleep(6)
+print("Why are you still here? The test is over, you passed. Probably.")
+time.sleep(1)
+print("...")
+time.sleep(2)
+print("Well, I guess you might as well help me with something while you're here.")
+time.sleep(2)
+print("Wanna do my math homework for me? :) Please, I don't wanna do it")
+bonus1 = input("Yes/ No ")
+print(bonus1)
+if bonus1 == "No":
+  print("Wrong. Here it is!")
+if bonus1 == "Yes":
+  print("Awesome. Take it!")
+time.sleep(2)
+print("Aight, have fun, I'm going to drink tea. Adios!")
